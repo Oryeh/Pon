19cf1fbcf7ac4cbea179c7746deed666ab125e57
Oryeh <93438055+Oryeh@users.noreply.github.com>
2023-12-01 19:21:54 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const gradients = [
+        "linear-gradient(to bottom right, #FF8EFC, #FBFBFB)",
+        "linear-gradient(to bottom right, #90EE90, #6A0572)",
+        "linear-gradient(to bottom right, #FFD700, #FFA500)"
+    ];
+
+    let currentGradientIndex = 0;
+
+    const infoBox = document.querySelector(".info-box");
+
+    function setNextGradient() {
+        currentGradientIndex = (currentGradientIndex + 1) % gradients.length;
+        infoBox.style.background = gradients[currentGradientIndex];
+    }
+
+    // Переключение фона при клике на .info-box
+    infoBox.addEventListener("click", setNextGradient);
+
+    // Начальный фон
+    infoBox.style.background = gradients[currentGradientIndex];
+});
