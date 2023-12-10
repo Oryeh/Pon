406491cd9cbe38a421d02e87eb9bd043ccf59dbe
Oryeh <93438055+Oryeh@users.noreply.github.com>
2023-12-10 16:26:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     infoBox.style.display = 'block';
 
     for (let i = 0; i < 50; i++) {
-        createStar();
+        createSnowflake();
     }
 });
 
-function createStar() {
-    const star = document.createElement('div');
-    star.className = 'star';
-    star.style.background = getRandomColor();
-    star.style.left = `${Math.random() * window.innerWidth}px`;
-    star.style.top = `${Math.random() * window.innerHeight}px`;
+function createSnowflake() {
+    const snowflake = document.createElement('div');
+    snowflake.className = 'snowflake';
 
-    document.body.appendChild(star);
-}
+    const size = Math.random() * 3 + 1; // Размер снежинки от 1 до 4
+    snowflake.style.width = `${size}px`;
+    snowflake.style.height = `${size}px`;
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+    snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`; // Длительность анимации от 2 до 5 секунд
+
+    document.body.appendChild(snowflake);
 }
