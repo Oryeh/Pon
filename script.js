0af1d8a92fd7199f0f90dbed4e7755aadbe55aae
Oryeh <93438055+Oryeh@users.noreply.github.com>
2023-12-10 14:47:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const infoBox = document.getElementById('infoBox');
+    infoBox.style.display = 'block';
+    infoBox.style.animation = 'fadeIn 1s ease-out';
+
+    for (let i = 0; i < 50; i++) {
+        createStar();
+    }
+});
+
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.background = getRandomColor();
+    star.style.left = `${Math.random() * window.innerWidth}px`;
+    star.style.top = `${Math.random() * window.innerHeight}px`;
+
+    document.body.appendChild(star);
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
